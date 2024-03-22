ebd033cd13122e42191c36238a299b36f02c8e48
ZhannaSaltanova <nedikovazhanna@gmail.com>
2024-03-22 14:15:26 +0200

    fix

--- script/script.js
+++ script/script.js
@@ -9,3 +9,27 @@ $(document).ready(function() {
       $('.burgermenu').removeClass('burgermenu_active');
     });
   });
+
+let images = ["../img/banner/AnyConv.com__sl1.webp", "../img/banner/AnyConv.com__sl2.webp"]; 
+let currentImageIndex = 0;
+
+function changeImage() {
+    currentImageIndex = (currentImageIndex + 1) % images.length;
+    document.getElementById('slideImg').src = images[currentImageIndex];
+}
+
+setInterval(changeImage, 3000); 
+
+document.getElementById('newestButton').addEventListener('click', function() {
+  document.getElementById('newestContent').classList.add('active');
+  document.getElementById('jackpotsContent').classList.remove('active');
+  this.classList.add('active');
+  document.getElementById('jackpotsButton').classList.remove('active');
+});
+
+document.getElementById('jackpotsButton').addEventListener('click', function() {
+  document.getElementById('jackpotsContent').classList.add('active');
+  document.getElementById('newestContent').classList.remove('active');
+  this.classList.add('active');
+  document.getElementById('newestButton').classList.remove('active');
+});
